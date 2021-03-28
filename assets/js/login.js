fd151696436e6345b4b41a75de26eafafdaaf1f5
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-03-28 17:18:44 +0800

    update js for login and sign up

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,33 @@
-function login() {
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
+$(function(){
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Signed in
-            var user = userCredential.user;
-            // ...
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            alert("Error : " + errorMessage);
-        });
-}
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          window.location.href = "/account.html";
+        } else {
+          // No user is signed in.
+        }
+      });
+
+    $("#login").click(function(){
+        login();
+    });
+
+    function login() {
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+    
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                alert("Error : " + errorMessage);
+            });
+    }
+});
+
