0e36024e6bfb8c21867e7714d4d641bc1d875eae
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-03-28 21:49:38 +0800

    remove ./ replace with / to make it website only

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,75 +1,82 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in. redirect to profile
-      window.location.href = "/account.html";
-    } else {
-      // No user is signed in.
+$(function(){
+    $("#sign-up").click(function(e){
+        e.preventDefault();
+        signup();
+    });
+    
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in. redirect to profile
+          window.location.href = "/account.html";
+        } else {
+          // No user is signed in.
+        }
+    });
+    
+    function writeUserData(userId, name) {
+        db.collection("users").doc(userId).set({
+            "username" : name
+        })
+            .then(() => {
+                ///console.log("Document successfully written!");
+                window.location.href = "/login.html";
+            })
+            .catch((error) => {
+                console.error("Error writing document: ", error);
+            });
+    
+    }
+    
+    function signup() {
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+        let username = document.getElementById("username").value;
+    
+        let checkPolicy = document.getElementById("checkPolicy").checked;
+        if(checkPolicy && email != "" && password != "" && username != ""){
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in 
+                var user = userCredential.user;
+                
+                //write username into users profile
+                writeUserData(user.uid, username);
+            })
+            .catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                // ..
+                alert("Error : " + errorMessage);
+            });
+        }else {
+            if(!checkPolicy){
+                alert("Please agree the privacy policy to continue !");
+            }
+    
+            if(username == ""){
+                alert("Please key in the username.");
+            }
+    
+            if(email == ""){
+                alert("Please key in an email.");
+            }
+    
+            if(password == ""){
+                alert("Please key in the password.");
+            }
+        }  
+        /*
+        let testuser = db.collection("users").doc("user.uid");
+        testuser.get().then((doc) => {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });*/
+        
     }
 });
-
-function writeUserData(userId, name) {
-    db.collection("users").doc(userId).set({
-        "username" : name
-    })
-        .then(() => {
-            ///console.log("Document successfully written!");
-            window.location.href = "/login.html";
-        })
-        .catch((error) => {
-            console.error("Error writing document: ", error);
-        });
-
-}
-
-function signup() {
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    let username = document.getElementById("username").value;
-
-    let checkPolicy = document.getElementById("checkPolicy").checked;
-    if(checkPolicy && email != "" && password != "" && username != ""){
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Signed in 
-            var user = userCredential.user;
-            
-            //write username into users profile
-            writeUserData(user.uid, username);
-        })
-        .catch((error) => {
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            // ..
-            alert("Error : " + errorMessage);
-        });
-    }else {
-        if(!checkPolicy){
-            alert("Please agree the privacy policy to continue !");
-        }
-
-        if(username == ""){
-            alert("Please key in the username.");
-        }
-
-        if(email == ""){
-            alert("Please key in an email.");
-        }
-
-        if(password == ""){
-            alert("Please key in the password.");
-        }
-    }  
-    /*
-    let testuser = db.collection("users").doc("user.uid");
-    testuser.get().then((doc) => {
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-    }).catch((error) => {
-        console.log("Error getting document:", error);
-    });*/
-    
-}
