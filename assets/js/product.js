62615b176c33766b019e9e2142b7d1771a8bd5ed
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-03-31 22:13:16 +0800

    bug fix

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,24 +1,11 @@
 $(function () {
-    //let product = "./assets/json/product.json";
-
-    /*$.getJSON(product).done(function (data) {
-        for (let i = 0; i < data.product.length; i++) {
-            $("#product").append('<div class="card product">' +
-                '<img src="' + data.product[i].img[0] + '" alt="' + data.product[i].name + '">' +
-                '<div class="card-body">' +
-                '<h5>' + data.product[i].name + '</h5>' + ' RM' + data.product[i].price +
-                '<p class="card-text">' + data.product[i].desc.substring(0, 105) + '...' + '</p>' +
-                '<a href="' + '/product.html?id=' + i + '" class="btn btn-info" target="_blank">View More</a>' +
-                '</div></div>');
-        }
-    });*/
     for (let i = 0; i < product.length; i++) {
         $("#product").append('<div class="card product">' +
             '<img src="' + product[i].img[0] + '" alt="' + product[i].name + '">' +
             '<div class="card-body">' +
             '<h5>' + product[i].name + '</h5>' + ' RM' + product[i].price +
             '<p class="card-text">' + product[i].desc.substring(0, 105) + '...' + '</p>' +
-            '<a href="' + './product.html?id=' + products[i].id + '" class="btn btn-info" target="_blank">View More</a>' +
+            '<a href="' + './product.html?id=' + product[i].id + '" class="btn btn-info" target="_blank">View More</a>' +
             '</div></div>');
     }
 });
