fa795caafecd7dc198e95d2f4fcc7965034029e9
haoyihyap <79739179+haoyihyap@users.noreply.github.com>
2021-04-01 09:53:52 +0800

    cart editing

--- assets/js/cartShow.js
+++ assets/js/cartShow.js
@@ -1,3 +1,23 @@
 $(function() {
 
+    if (checkBrowser()) {
+        var i;
+
+        if (localStorage.length > 0) {
+            for (i = 0; i < localStorage.length; i++) {
+                //retrieve from local storage
+                var product_img = localStorage.getItem("product-img");
+                var product_name = localStorage.getItem("product-name");
+                var product_price = localStorage.getItem("product-price");
+                var product_quantity = localStorage.getItem("product-quantity");
+
+                //show the item
+                $("#item_added").append(
+                    '<div class="row"> <div class="col-sm-4">' + product_img + '</div>' +
+                    '<div class="col-sm-8">' + '<div>' + product_name + '</div>' + '<div> RM ' + product_price + '</div>' + '<div>' + '<span><i class="fas fa-plus"></i></span><span>' + product_quantity + '</span><span><i class="fas fa-minus"></i></span>' + '</div>' + '</div>' + '</div>');
+            }
+        } else {
+            $("#item_added").html() = '<div class="row">There are no item item selected</div>';
+        }
+    }
 });
