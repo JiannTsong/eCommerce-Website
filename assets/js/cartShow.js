2cb578faf1acaaeb9c6dfae22114bc1a961d0091
haoyihyap <79739179+haoyihyap@users.noreply.github.com>
2021-04-02 15:56:51 +0800

    Update cartShow.js

--- assets/js/cartShow.js
+++ assets/js/cartShow.js
@@ -12,7 +12,7 @@ $(function() {
                 $("#item_added").append(
                     '<div class="row" data-cartProduct' + (i + 1) + '> <div class="col-sm-4">' + '<img src="' + product[id].img[0] + '" alt="' + product[id].name + '">' + '</div>' +
                     '<div class="col-sm-8">' + '<div><b>' + product[id].name.substring(0, 30) + '</b></div>' +
-                    '<div> RM ' + product[id].price + '</div>' + '<div>' + '<span><i class="fas fa-plus"></i></span><span>' + product_quantity + '</span><span><i class="fas fa-minus"></i></span>' + '</div>' + '</div>' + '</div>');
+                    '<div> RM ' + product[id].price + '</div>' + '<div>' + '<span><i class="fas fa-plus"></i></span><span>' + product_quantity + '</span><span><i class="fas fa-minus"></i></span>' + '</div>' + '<div id = "deleteItem"><i class="fas fa-trash-alt"></i></div>' + '</div>');
             }
         } else {
             $("#item_added").html() = '<div class="row">There are no item item selected</div>';
