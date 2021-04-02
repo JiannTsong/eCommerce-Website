61afb2abe60ddbe0ca94ceb11b4677fcf72a78fc
haoyihyap <79739179+haoyihyap@users.noreply.github.com>
2021-04-02 19:06:58 +0800

    cart edit

--- assets/js/cartShow.js
+++ assets/js/cartShow.js
@@ -1,8 +1,12 @@
 $(function() {
 
+    $("#message").hide();
     //show item store in local storage
     if (typeof(Storage) !== "undefined") {
         if (JSON.parse(localStorage.getItem("Cart")) != null) {
+            $("#itemAdded").show();
+            $("#message").hide();
+
             for (var i = 0; i < localStorage.length; i++) {
                 //retrieve from local storage
                 cartArray = JSON.parse(localStorage.getItem(localStorage.key(i)));
@@ -10,24 +14,40 @@ $(function() {
                 var product_quantity = cartArray.product_quantity;
 
                 //show the item
-                $("#item_added").append(
+                $("#itemAdded").append(
                     '<div class="row"> <div class="col-sm-4">' + '<img src="' + product[id].img[0] + '" alt="' + product[id].name + '">' + '</div>' +
                     '<div class="col-sm-8">' + '<div><b>' + product[id].name.substring(0, 30) + '</b></div>' +
-                    '<div> RM ' + product[id].price + '</div>' + '<div>' + '<span><i class="fas fa-plus"></i></span><span>' + product_quantity + '</span><span><i class="fas fa-minus"></i></span><span id = "deleteItem"><i class="fas fa-trash-alt"></i></span></div></div>');
+                    '<div> RM ' + product[id].price + '</div>' + '<div>' + '<span id = "add"><i class="fas fa-plus"></i></span><span>' + product_quantity + '</span><span id = "deduct"><i class="fas fa-minus"></i></span><span id = "deleteItem"><i class="fas fa-trash-alt"></i></span></div></div>');
             }
         } else {
-            $("#item_added").html() = '<div class="row">There are no item item selected</div>';
+            $("#itemAdded").hide();
+            $("#message").show().html("Your shopping cart is empty. Enjoy your shopping!");
         }
+    } else {
+        $("#itemAdded").hide();
+        $("#message").show().html("Your shopping cart is empty. Enjoy your shopping!");
     }
 
     //delete the item when user click delete
     $("#deleteItem").click(function() {
-        localStorage.removeItem("");
-        location.reload();
+        localStorage.removeItem(localStorage.key());
+        //location.reload();
     });
 
     //clear all cart when user click clear all
     $("#clear").click(function() {
         localStorage.clear();
     });
+
+    //add quantity
+    $("itemAdded").on("click", '$("#add")', function() {
+
+    });
+
+    //deduct quantity
+    $("itemAdded").on("click", '$("#deduct")', function() {
+
+        // if(quanity == 0 ) invoke delete item
+    });
+
 });
