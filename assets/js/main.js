de55645702c478f04263d8d97e3aa79ead3e2ab0
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-04-03 21:12:52 +0800

    Add WhatsApp Button

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,17 @@ $(function() {
     } else {
         $("#cart-count").html(0);
     }
+
+    (function () {
+        var options = {
+            whatsapp: "6013-25984966", // WhatsApp number
+            call_to_action: "Message us", // Call to action
+            position: "right", // Position may be 'right' or 'left'
+            pre_filled_message: "Hi", // WhatsApp pre-filled message
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
 });
