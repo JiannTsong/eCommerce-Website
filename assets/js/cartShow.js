2b97981d84aa20f288313ca81ce46866dd18d15c
haoyihyap <79739179+haoyihyap@users.noreply.github.com>
2021-04-03 20:30:08 +0800

    Update cartShow.js

--- assets/js/cartShow.js
+++ assets/js/cartShow.js
@@ -22,7 +22,7 @@ $(function() {
                     '<div class="col-sm-8">' + '<div><b>' + product[pid].name.substring(0, 30) + '</b></div>' +
                     '<div>' + product[pid].desc + '</div>' +
                     '<div class="cartprice"> RM ' + product[pid].price + '</div>' +
-                    '<div>' + '<span id = "add"><i class="fas fa-plus iconwidth"></i></span><span><input type="number" id = "qty" value = "' + product_quantity + '"></span><span id = "deduct"><i class="fas fa-minus iconwidth2"></i></span><span id = "deleteItem"><i class="fas fa-trash-alt"></i></span></div></div>');
+                    '<div>' + '<span id = "add" style = "padding-right: 10px;"><i class="fas fa-plus iconwidth"></i></span><span><input type="number" id = "qty" value = "' + product_quantity + '" style = "width: 3.5rem;" min = "0" max = "' + product[pid].stock + '"></span><span id = "deduct" style = "padding-right: 10px; padding-left: 10px;"><i class="fas fa-minus iconwidth2"></i></span><span id = "deleteItem"><i class="fas fa-trash-alt"></i></span></div></div>');
             }
         } else {
             $("#itemAdded").hide();
@@ -33,32 +33,38 @@ $(function() {
         $("#message").show().html("Your shopping cart is empty. Enjoy your shopping!");
     }
 
-    //delete the item when user click delete
-    $("#deleteItem").click(function() {
-        localStorage.removeItem(localStorage.key());
-        //location.reload();
-    });
-
     //clear all cart when user click clear all
     $("#clear").click(function() {
         localStorage.removeItem("Cart");
         location.reload();
     });
 
-    //add quantity
-    $("itemAdded").on("click", $("#add"), function() {
-        alert("add");
+    //change on effect - quantity
+    $("#itemAdded").on('change', $("#qty"), function(e) {
+        e.preventDefault();
+        alert("Changes");
     });
 
-    //correct way here
-    $("#add").click(function() {
+    //add quantity
+    $("#itemAdded").on("click", $("#add"), function(e) {
+        e.preventDefault();
         alert("add");
     });
 
     //deduct quantity
-    $("itemAdded").on("click", $("#deduct"), function() {
-        alert("add");
+    $("#itemAdded").on("click", $("#deduct"), function(e) {
+        e.preventDefault();
+        alert("deduct");
         // if(quanity == 0 ) invoke delete item
+    });
+
+    //delete the item when user click delete
+    $("#itemAdded").on("click", $("#deleteItem"), function(e) {
+        e.preventDefault();
+        alert("delete");
+        // if(quanity == 0 ) invoke delete item
+        // localStorage.removeItem(localStorage.key());
+        //location.reload();
     });
 
 });
