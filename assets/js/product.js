aec796d3b1e51cf167baef4a55a4298318cce0aa
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-04-03 17:27:20 +0800

    Fix firefox responsive issues

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,7 +1,7 @@
 $(function () {
 
     for (let i = product.length - 1; i >= 0; i--) {
-        $("#product").append('<div class="card product" style="height:30rem;">' +
+        $("#product").append('<div class="card product" style="min-height:30rem;">' +
             '<img src="' + product[i].img[0] + '" alt="' + product[i].name + '">' +
             '<div class="card-body">' +
             '<h6 class="class-title">' + product[i].name.substring(0, 35) + '</h6>' + ' RM ' + product[i].price +
