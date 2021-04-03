c0a07af82b1163a412312b439b5d1d2304d34dba
haoyihyap <79739179+haoyihyap@users.noreply.github.com>
2021-04-04 00:00:18 +0800

    Update cartShow.js

--- assets/js/cartShow.js
+++ assets/js/cartShow.js
@@ -13,14 +13,14 @@ $(function() {
                 //retrieve from local storage
 
                 let pid = parseInt(cartArray[i].product_ID) - 1;
-                console.log(pid);
+
                 let product_quantity = cartArray[i].product_quantity;
 
                 //show the item
                 $("#itemAdded").append(
-                    '<div class="row"> <div class="col-sm-4">' + '<img src="' + product[pid].img[0] + '" alt="' + product[pid].name + '" class = "imgcart">' + '</div>' +
-                    '<div class="col-sm-8">' + '<div><b>' + product[pid].name.substring(0, 30) + '</b></div>' +
-                    '<div>' + product[pid].desc + '</div>' +
+                    '<div class="row" data-cartid = "' + pid + '" style = "padding-top: 50px"> <div class="col-sm-4">' + '<img src="' + product[pid].img[0] + '" alt="' + product[pid].name + '" class = "imgcart">' + '</div>' +
+                    '<div class="col-sm-8">' + '<div style = "font-size: 20px;"><b>' + product[pid].name.substring(0, 30) + '</b></div>' +
+                    '<div><i style = "justify-content: space-evenly;">' + product[pid].desc + '</i></div>' +
                     '<div class="cartprice"> RM ' + product[pid].price + '</div>' +
                     '<div>' + '<span id = "add" style = "padding-right: 10px;"><i class="fas fa-plus iconwidth"></i></span><span><input type="number" id = "qty" value = "' + product_quantity + '" style = "width: 3.5rem;" min = "0" max = "' + product[pid].stock + '"></span><span id = "deduct" style = "padding-right: 10px; padding-left: 10px;"><i class="fas fa-minus iconwidth2"></i></span><span id = "deleteItem"><i class="fas fa-trash-alt"></i></span></div></div>');
             }
@@ -42,29 +42,120 @@ $(function() {
     //change on effect - quantity
     $("#itemAdded").on('change', $("#qty"), function(e) {
         e.preventDefault();
-        alert("Changes");
+        var cartid = $(this).data("cartid");
+        console.log(cartid);
+
+        var newQty = $("#qty").val();
+        var cartArray = JSON.parse(localStorage.getItem("Cart"));
+
+        if (newQty !== 0) {
+            for (var i = 0; i < cartArray.length; i++) {
+                if (cartid === cartArray[i].product_ID) {
+                    cartArray[i].product_quantity = newQty;
+
+                    localStorage.setItem("Cart", JSON.stringify(cartArray));
+
+                    alert("Item have been successfully modify");
+                } else
+                    alert("Something went wrong! Please try again.");
+            }
+        } else if (newQty === 0) {
+            for (var i = 0; i < cartArray.length; i++) {
+                if (cartid === cartArray[i].product_ID) {
+
+                    delete cartArray[i].product_ID;
+                    delete cartArray[i].product_quantity;
+
+                    localStorage.setItem("Cart", JSON.stringify(cartArray));
+
+                    alert("Item have been successfully modify");
+                } else
+                    alert("Something went wrong! Please try again.");
+            }
+        } else {
+            alert("Something went wrong! Please try again.")
+        }
+
+        location.reload();
     });
 
     //add quantity
     $("#itemAdded").on("click", $("#add"), function(e) {
         e.preventDefault();
-        alert("add");
+        var cartid = $(this).data("cartid");
+        console.log(cartid);
+
+        var cartArray = JSON.parse(localStorage.getItem("Cart"));
+
+        for (var i = 0; i < cartArray.length; i++) {
+            if (cartid === cartArray[i].product_ID) {
+                cartArray[i].product_quantity += 1;
+
+                localStorage.setItem("Cart", JSON.stringify(cartArray));
+
+                alert("Item have been successfully added");
+            } else
+                alert("Something went wrong! Please try again.");
+        }
+
+        location.reload();
     });
 
     //deduct quantity
     $("#itemAdded").on("click", $("#deduct"), function(e) {
-        e.preventDefault();
-        alert("deduct");
-        // if(quanity == 0 ) invoke delete item
+        var cartid = $(this).data("cartid");
+        console.log(cartid);
+
+        var cartArray = JSON.parse(localStorage.getItem("Cart"));
+
+        for (var i = 0; i < cartArray.length; i++) {
+            if (cartid === cartArray[i].product_ID) {
+                cartArray[i].product_quantity -= 1;
+
+                if (cartArray[i].product_quantity !== 0) {
+                    localStorage.setItem("Cart", JSON.stringify(cartArray));
+
+                    alert("Item have been successfully modify");
+                } else if (cartArray[i].product_quantity === 0) {
+
+                    delete cartArray[i].product_ID;
+                    delete cartArray[i].product_quantity;
+
+                    localStorage.setItem("Cart", JSON.stringify(cartArray));
+
+                    alert("Item have been successfully deleted");
+                } else
+                    alert("Something went wrong! Please try again.")
+            } else
+                alert("Something went wrong! Please try again.");
+        }
+
+        location.reload();
     });
 
     //delete the item when user click delete
     $("#itemAdded").on("click", $("#deleteItem"), function(e) {
         e.preventDefault();
-        alert("delete");
-        // if(quanity == 0 ) invoke delete item
-        // localStorage.removeItem(localStorage.key());
-        //location.reload();
+        var cartid = $(this).data("cartid");
+        console.log(cartid);
+
+        var cartArray = JSON.parse(localStorage.getItem("Cart"));
+
+
+        for (var i = 0; i < cartArray.length; i++) {
+            if (cartid === cartArray[i].product_ID) {
+
+                delete cartArray[i].product_ID;
+                delete cartArray[i].product_quantity;
+
+                localStorage.setItem("Cart", JSON.stringify(cartArray));
+
+                alert("Item have been successfully deleted");
+            } else
+                alert("Something went wrong! Please try again.");
+        }
+
+        location.reload();
     });
 
 });
