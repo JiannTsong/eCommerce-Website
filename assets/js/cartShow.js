742ec4dae51cdc8c7877c873689d493c41ed73a7
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-04-04 14:33:05 +0800

    Update Cart Link Color

--- assets/js/cartShow.js
+++ assets/js/cartShow.js
@@ -18,8 +18,8 @@ $(function() {
 
                 //show the item
                 $("#itemAdded").append(
-                    '<div class="row" data-cartid = "' + (pid+1) + '" style = "padding-top: 50px"> <div class="col-sm-4">' + '<a href="product.html?id='+(pid+1)+'"><img src="' + product[pid].img[0] + '" alt="' + product[pid].name + '" class = "imgcart"></a>' + '</div>' +
-                    '<div class="col-sm-8">' + '<div style = "font-size: 20px;"><a href="product.html?id='+(pid+1)+'"><b>' + product[pid].name.substring(0, 30) + '</b></a></div>' +
+                    '<div class="row" data-cartid = "' + (pid+1) + '" style = "padding-top: 50px"> <div class="col-sm-4">' + '<a href="product.html?id='+(pid+1)+'"><img src="' + product[pid].img[0] + '" alt="' + product[pid].name + '" class="imgcart"></a>' + '</div>' +
+                    '<div class="col-sm-8">' + '<div style = "font-size: 20px;"><a href="product.html?id='+(pid+1)+'" class="cart_link"><b>' + product[pid].name.substring(0, 30) + '</b></a></div>' +
                     '<div><i style = "justify-content: space-evenly;">' + product[pid].desc + '</i></div>' +
                     '<div class="cartprice"> RM ' + product[pid].price + '</div>' +
                     '<div>' + '<span id = "add" style = "padding-right: 10px;"><i class="fas fa-plus iconwidth"></i></span><span><input type="number" id = "qty" value = "' + product_quantity + '" style = "width: 3.5rem;" min = "0" max = "' + product[pid].stock + '"></span><span id = "deduct" style = "padding-right: 10px; padding-left: 10px;"><i class="fas fa-minus iconwidth2"></i></span><span id = "deleteItem"><i class="fas fa-trash-alt"></i></span></div></div>');
