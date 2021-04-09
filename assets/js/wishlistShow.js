14764454baea479daf52ab575f4a2b5b3671c3de
haoyihyap <79739179+haoyihyap@users.noreply.github.com>
2021-04-09 13:53:04 +0800

    wishlist modify

--- assets/js/wishlistShow.js
+++ assets/js/wishlistShow.js
@@ -6,23 +6,21 @@ $(function() {
     let aPI = "assets/json/product_details.json";
     $.getJSON(aPI).done(function(product) {
         if (typeof(Storage) !== "undefined") {
-            if (JSON.parse(localStorage.getItem("Cart")) != null) {
+            if (JSON.parse(localStorage.getItem("Wishlist")) != null) {
                 $("#itemAdded").show();
                 $("#message").hide();
 
-                let cartArray = JSON.parse(localStorage.getItem("Cart"));
+                let wishlistArray = JSON.parse(localStorage.getItem("Wishlist"));
 
-                for (let i = 0; i < cartArray.length; i++) {
+                for (let i = 0; i < wishlistArray.length; i++) {
+
                     //retrieve from local storage
-
-                    let pid = parseInt(cartArray[i].product_ID) - 1;
-
-                    let product_quantity = cartArray[i].product_quantity;
+                    let pid = parseInt(wishlistArray[i]) - 1;
 
                     //show the item
                     $("#itemAdded").append(
 
-                        '<div class="row" data-cartid = "' + (pid + 1) + '" style = "padding-top: 50px"> <div class="col-sm-4">' + '<a href="product.html?id=' + (pid + 1) + '"><img src="' + product[pid].img[0] + '" alt="' + product[pid].name + '" class="imgcart"></a>' + '</div>' +
+                        '<div class="row" data-wishlistid = "' + (pid + 1) + '" style = "padding-top: 50px"> <div class="col-sm-4">' + '<a href="product.html?id=' + (pid + 1) + '"><img src="' + product[pid].img[0] + '" alt="' + product[pid].name + '" class="imgcart"></a>' + '</div>' +
                         '<div class="col-sm-8">' + '<div style = "font-size: 20px;"><a href="product.html?id=' + (pid + 1) + '" class="cart_link"><b>' + product[pid].name.substring(0, 30) + '</b></a></div>' +
                         '<div><p style = "font-style:italic;justify-content: stretch;text-align: justify; text-justify: inter-word;">' + product[pid].desc + '</p></div>' +
                         '<div class="cartprice"> RM ' + product[pid].price + '</div>' +
@@ -31,58 +29,55 @@ $(function() {
                 }
             } else {
                 $("#itemAdded").hide();
-                $("#message").show();
+                $("#message").show().html("There are no items in your wishlist");
             }
         } else {
             $("#itemAdded").hide();
-            $("#message").show();
+            $("#message").show().html("There are no items in your wishlist");
         }
 
         //clear all cart when user click clear all
         $("#clear").click(function() {
-            localStorage.removeItem("Cart");
+            localStorage.removeItem("Wishlist");
             location.reload();
         });
 
         //delete the item when user click delete
         $("#itemAdded > div.row").on("click", "#deleteItem", function(e) {
             e.preventDefault();
-            let cartid = $(this).closest("div.row").data("cartid");
-            console.log(cartid);
+            let wishlistid = $(this).closest("div.row").data("wishlistid");
 
-            let cartArray = JSON.parse(localStorage.getItem("Cart"));
+            let wishlistArray = JSON.parse(localStorage.getItem("Wishlist"));
 
 
-            for (let i = 0; i < cartArray.length; i++) {
-                if (cartid == cartArray[i].product_ID) {
-                    delete cartArray[i]; //delete the item from array
+            for (let i = 0; i < wishlistArray.length; i++) {
+                if (wishlistid == wishlistArray[i]) {
+                    delete wishlistArray[i]; //delete the item from array
 
                     let filtered = []; //declare array list var
 
                     //recreate array list without null element
-                    filtered = cartArray.filter(function(el) {
+                    filtered = wishlistArray.filter(function(el) {
                         return el != null;
                     });
 
                     //remove array if no more element in array
                     if (filtered.length == 0) {
-                        localStorage.removeItem("Cart");
+                        localStorage.removeItem("Wishlist");
                     } else {
-                        localStorage.setItem("Cart", JSON.stringify(filtered));
+                        localStorage.setItem("Wishlist", JSON.stringify(filtered));
                     }
 
                     console.log("Item have been successfully deleted");
                 }
             }
-
-            location.reload();
         });
 
         //add to cart
         $("#itemAdded > div.row").on("click", "#addtocart", function(e) {
             e.preventDefault();
 
-            let cartid = $(this).closest("div.row").data("cartid");
+            let wishlistid = $(this).closest("div.row").data("wishlistid");
 
 
             if (typeof(Storage) !== "undefined") {
@@ -96,7 +91,7 @@ $(function() {
                 //loop in cart to found and update
                 let updateStatus = false;
                 for (let f = 0; f < cartArray.length; f++) {
-                    if (cartArray[f].product_ID == id) {
+                    if (cartArray[f].product_ID == wishlistid) {
                         cartArray[f].product_quantity = cartArray[f].product_quantity + 1;
                         updateStatus = true;
                     }
@@ -105,7 +100,7 @@ $(function() {
                 //if not found after loop then create new
                 if (!updateStatus) {
                     let cart = new Object();
-                    cart.product_ID = cartid;
+                    cart.product_ID = wishlistid;
                     cart.product_quantity = 1;
 
                     //push item into array
