37f97b2c09e6fac92c32ff0ee073649e90b47070
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-04-09 08:45:47 +0800

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,8 +1,5 @@
 // This is the service worker with the Cache-first network
-
-//make the cache only limit to every month
-let date =  new Date();
-const CACHE = "v1"+date.getFullYear() + date.getMonth();
+const CACHE = "v1";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
