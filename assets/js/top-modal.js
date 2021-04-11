830a2dadbf597ea6a7daf6f7863cccba377a5697
haoyihyap <79739179+haoyihyap@users.noreply.github.com>
2021-04-11 15:03:55 +0800

    change side modal to non-display when is mobile

--- assets/js/top-modal.js
+++ assets/js/top-modal.js
@@ -24,45 +24,70 @@ $(function() {
         return "";
     }
 
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+
     $.getJSON("assets/json/product_details.json").done(function(product_data) {
 
-        if (getCookie("hide-banner") === false || getCookie("hide-banner") === null || getCookie("hide-banner") === "") {
-            var topProductID = [3, 7, 9];
+        if (isMobile.any() === null) {
+            if ((getCookie("hide-banner") === false || getCookie("hide-banner") === null || getCookie("hide-banner") === "")) {
+                var topProductID = [3, 7, 9];
 
-            //retrieve img from cookies to prsent as modal
-            //show image of product
-            let count_carousel = 1;
-            for (let j = 0; j < topProductID.length; j++) {
+                //retrieve img from cookies to prsent as modal
+                //show image of product
+                let count_carousel = 1;
+                for (let j = 0; j < topProductID.length; j++) {
 
-                pid = topProductID[j];
-                $("#sidemodal").show();
+                    pid = topProductID[j];
+                    $("#sidemodal").show();
 
-                for (let i = 0; i < product_data.length; i++) {
+                    for (let i = 0; i < product_data.length; i++) {
 
-                    if (product_data[i].id == topProductID[j]) {
+                        if (product_data[i].id == topProductID[j]) {
 
-                        if (count_carousel == 1) {
-                            count_carousel++;
-                            console.log("First" + product_data[i].img[0]);
-                            $("body>div.sidemodal").show();
-                            $("div#topProduct").prepend(
-                                '<div class="carousel-item active">' +
-                                '<a href="product.html?id=' + (i + 1) + '">' +
-                                '<img class="d-block w-100" src="' + product_data[i].img[0] + '" alt="' + product_data[i].name + '" style="height: 350px"></a></div>'
-                            );
-                        } else {
-                            count_carousel++;
-                            console.log("Not first" + product_data[i].img[0]);
-                            $("#topProduct").prepend(
-                                '<div class="carousel-item">' +
-                                '<a href="product.html?id=' + (i + 1) + '">' +
-                                '<img class="d-block w-100" id="imgsize2" src="' + product_data[i].img[0] + '" alt="' + product_data[i].name + '" style="height: 350px"></a></div>'
-                            );
+                            if (count_carousel == 1) {
+                                count_carousel++;
+                                console.log("First" + product_data[i].img[0]);
+                                $("body>div.sidemodal").show();
+                                $("div#topProduct").prepend(
+                                    '<div class="carousel-item active">' +
+                                    '<a href="product.html?id=' + (i + 1) + '">' +
+                                    '<img class="d-block w-100" src="' + product_data[i].img[0] + '" alt="' + product_data[i].name + '" style="height: 350px"></a></div>'
+                                );
+                            } else {
+                                count_carousel++;
+                                console.log("Not first" + product_data[i].img[0]);
+                                $("#topProduct").prepend(
+                                    '<div class="carousel-item">' +
+                                    '<a href="product.html?id=' + (i + 1) + '">' +
+                                    '<img class="d-block w-100" id="imgsize2" src="' + product_data[i].img[0] + '" alt="' + product_data[i].name + '" style="height: 350px"></a></div>'
+                                );
+                            }
                         }
                     }
                 }
-            }
-        } else if (getCookie("hide-banner") === true)
+            } else if (getCookie("hide-banner") === true)
+                $("body>div.sidemodal").hide();
+        } else
             $("body>div.sidemodal").hide();
     });
 
