8e714d30bbb6f6bd97e0d301ec672f071ef7d196
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-04-11 11:01:53 +0800

    add order detail page

--- assets/js/orders.js
+++ assets/js/orders.js
@@ -1,4 +1,40 @@
 $(function () {
+
+    function orderPrinter(data) { 
+        $.getJSON("assets/json/product_details.json").done(function(product){
+            $('div.orders>div#no-orders').hide();
+            $("div.orders").append('<h6>Order #<strong>'+data.date+'</strong></h6>');
+            $("div.orders").append('<h6>Order Date : '+new Date(data.date)+'</h6>');
+
+            for(let i =0;i<data.product.length;i++){
+                for(let h=0; h<product.length;h++){
+                    if(product[h].id == data.product[i].pid){
+                        //print
+                        //console.log("Detect : " + product[h].id);
+                        $("div.orders").append(
+                        '<div class="row" data-cartid = "' + data.product[i].pid + '"> <div class="col-sm-3">' + '<a href="product.html?id=' + data.product[i].pid + '"><img src="' + product[h].img[0] + '" alt="' + product[h].name + '" class="imgcart"></a>' + '</div>' +
+                        '<div class="col-sm-9">' + '<div style = "font-size: 20px;"><a href="product.html?id=' + data.product[i].pid + '" class="cart_link"><b>' + product[h].name.substring(0, 30) + '</b></a></div>' +
+                        '<div class="cartprice" style="font-size:1rem;"> RM ' + data.product[i].price + '</div>' +
+                        'Product Quantity : ' + data.product[i].quantity) + '</div>';
+                    }
+                }
+            }
+            $('div.orders').append('<div class="row">'+
+                '<div class="col-sm-10">Shipping Fee</div>'+
+                '<div class="col-sm-1">RM</div>'+
+                '<div class="col-sm-1" style="text-align:right;">'+data.shipping+'</div>'+
+            '</div>');
+
+            $('div.orders').append('<div class="row">'+
+                '<div class="col-sm-10">Total</div>'+
+                '<div class="col-sm-1">RM</div>'+
+                '<div class="col-sm-1" style="text-align:right;">'+(data.total+10)+'</div>'+
+            '</div>');
+
+            $("div.orders").append('<hr style="margin:0rem 0rem 1rem 0rem;">');
+        });
+     }
+
     firebase.auth().onAuthStateChanged(function (user) {
         if (!user) {
             // No user is signed in.
@@ -7,7 +43,7 @@ $(function () {
         } else {
             //console.log(user);
 
-            var name, email, photoUrl, uid, emailVerified;
+            let name, email, photoUrl, uid, emailVerified;
             let user = firebase.auth().currentUser;
             if (user != null) {
                 name = user.displayName;
@@ -19,26 +55,14 @@ $(function () {
                 // you have one. Use User.getToken() instead.
             }
 
-            $("#uname").attr("value", name);
-            $("#uemail").text(email);
-
-            let uprofile = db.collection("users").doc(uid);
-            uprofile.get().then((doc) => {
-                if (doc.exists) {
-                    if (doc.data().address) {
-                        $("#ushipping").text(doc.data().address);
-                    } else {
-                        console.log("Address does not exists.");
-                    }
-
-                } else {
-                    // doc.data() will be undefined in this case
-                    console.log("No such document!");
-                }
-            }).catch((error) => {
-                console.log("Error getting document:", error);
+            db.collection("orders").doc(uid).get()
+            .then(snapshot => {
+                //console.log("snapshot"+JSON.stringify(snapshot.data().order_detail));
+                snapshot.data().order_detail.forEach(element => {
+                    orderPrinter(element);
+                    //console.log("firebase = "+JSON.stringify(element));
+                });
             });
-
         
         }
     });
